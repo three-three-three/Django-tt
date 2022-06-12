541824c4808b07dc3be11ec1e7ba3aa6b7eed157
three <daiertang9@gmail.com>
2022-06-12 16:26:53 +0800

    Login,Logout,Log_status,Signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,51 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('username', 'email')
+
+
+# 帮助我们去查看用户有没有输入username和password
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    # 括号里限制输入长度
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        # TODO<HOMEWORK> 增加验证 username 是不是只由给定的字符集合构成
+        # .lower()表示在存用户输入信息的时候就存小写
+        if User.objects.filter(username=data['username'].lower()).exists():
+            # 检查有没有重复，有重复就抛出一个异常
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from accounts.api.serializers import UserSerializer, LoginSerializer, SignupSerializer
+from django.contrib.auth import (
+    logout as django_logout,
+    login as django_login,
+    authenticate as django_authenticate,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,7 +17,103 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = permissions.IsAuthenticated
 
 
+class AccountViewSet(viewsets.ViewSet):
+    '''界面出现用户输入用户名和密码'''
+    serializer_class = SignupSerializer
 
+    """我只能用get去请求"""
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        """
+        查看用户当前的登录状态和具体信息
+        """
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        """
+        默认的 username 是 admin, password 也是 admin，
+
+        """
+        # 把用户的输入传给LoginSerializer检查
+        # request.data就是用户请求[POST]的数据
+        '''在开发中，凡是要检测用户输入，都是把request.data传到serializer中
+        ，然后调用is_valid()，错误会放在.errors中'''
+        serializer = LoginSerializer(data=request.data)
+        '''输入不合法，返回400 Bad Request'''
+        '''errors也是django serializer自带的，带有错误信息'''
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status=400)
+
+        '''validation is ok, login'''
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        '''User不存在就无法登录，抛出异常'''
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                "success": False,
+                "message": "User does not exit",
+            }, status=400)
+
+        user = django_authenticate(username=username, password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "username and password does not match",
+            }, status=400)
+
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data,
+        })
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        """
+        登出当前用户
+        """
+        django_logout(request)
+        return Response({"success": True})
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        """
+        使用 username, email, password 进行注册
+        """
+        '''对应 serializers 里面的 class meta限制了用户的输入信息'''
+        # 不太优雅的写法
+        # username = request.data.get('username')
+        # if not username:
+        #     return Response("username required", status=400)
+        # password = request.data.get('password')
+        # if not password:
+        #     return Response("password required", status=400)
+        # if User.objects.filter(username=username).exists():
+        #     return Response("password required", status=400)
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': "Please check input",
+                'errors': serializer.errors,
+            }, status=400)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data,
+        }, status=201)
