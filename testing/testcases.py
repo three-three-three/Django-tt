7cd5813c7dadf6206f442826962cfd6c18679f83
three <daiertang9@gmail.com>
2022-06-28 17:29:19 +0800

    correct some mistakes

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,7 +1,7 @@
-from django.test import TestCase as DjangoTestCase
+from comments.models import Comment
 from django.contrib.auth.models import User
+from django.test import TestCase as DjangoTestCase
 from rest_framework.test import APIClient
-
 from tweets.models import Tweet
 
 '''
@@ -23,6 +23,8 @@ class TestCase(DjangoTestCase):
     def create_user(self, username, email=None, password=None):
         if password is None:
             password = 'generic password'
+        if email is None:
+            email = '{}@gmail.com'.format(username)
         # 不能写成 User.objects.create()
         # 因为 password 需要被加密, username 和 email 需要进行一些 normalize 处理
         return User.objects.create_user(username, email, password)
@@ -31,6 +33,11 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
+    def create_comment(self, user, tweet, content=None):
+        if content is None:
+            content = 'default comment content'
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
 
 
 '''
