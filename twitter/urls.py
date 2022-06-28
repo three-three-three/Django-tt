ea4f5df210300d2ced3ee775401fef62fe088621
three <daiertang9@gmail.com>
2022-06-28 17:10:57 +0800

    create api for comments

--- twitter/urls.py
+++ twitter/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from accounts.api import views
+from comments.api.views import CommentViewSet
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from accounts.api import views
-from django.conf import settings
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from rest_framework import routers
 from tweets.api.views import TweetViewSet
 
 # 创建一个新的views要指定url路径
@@ -30,6 +31,7 @@ router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
