f6d2426a313614c33ab45d36f40daaca73d2d3f2
three <daiertang9@gmail.com>
2022-08-02 11:11:59 +0800

    Likes model and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from testing.testcases import TestCase
 from tweets.models import Tweet
 from datetime import timedelta
 from utils.time_helpers import utc_now
@@ -7,9 +7,24 @@ from utils.time_helpers import utc_now
 
 class TweetTests(TestCase):
 
+    def setUp(self):
+        self.linghu = self.create_user('linghu')
+        self.tweet = self.create_tweet(self.linghu, content='Jiuzhang Dafa Hao')
+
     def test_hours_to_now(self):
         tde = User.objects.create_user(username='tde')
         tweet = Tweet.objects.create(user=tde, content='good')
         tweet.created_at = utc_now() - timedelta(hours=10)
         tweet.save()
         self.assertEqual(tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        dongxie = self.create_user('dongxie')
+        self.create_like(dongxie, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
